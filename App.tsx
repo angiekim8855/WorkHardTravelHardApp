dd371938dc98b39c03d85e11ab6152b589892765
angiekim8855 <angiekim8855@gmail.com>
2025-04-09 21:42:04 +0900

    add: AsyncStorage에 데이터 저장

--- App.tsx
+++ App.tsx
@@ -1,26 +1,50 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView } from "react-native";
 import { theme } from "./colors";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface Todo {
     text: string;
-    work: boolean;
+    working: boolean;
 }
-
+const STORAGE_KEY = "@toDos";
 export default function App() {
     const [working, setWorking] = useState(true);
+
+    useEffect(() => {
+        loadTodos();
+    }, []);
     const work = () => setWorking(true);
     const travel = () => setWorking(false);
     const [text, setText] = useState("");
     const [todos, setTodos] = useState<Record<string, Todo>>({});
+    const saveTodos = async (newTodos: Record<string, Todo>) => {
+        try {
+            const jsonValue = JSON.stringify(newTodos);
+            await AsyncStorage.setItem(STORAGE_KEY, jsonValue);
+        } catch (e) {
+            alert("Failed to save todo");
+        }
+    };
+    const loadTodos = async () => {
+        try {
+            const jsonValue = await AsyncStorage.getItem(STORAGE_KEY);
+            if (jsonValue !== null) {
+                setTodos(JSON.parse(jsonValue));
+            }
+        } catch (e) {
+            alert("Failed to load todos. Try later");
+        }
+    };
 
     const onChangeText = (payload: any) => setText(payload);
-    const addTodo = () => {
+    const addTodo = async () => {
         if (text === "") return;
         // 입력한 텍스트를 투두에 추가
-        const newTodos = { ...todos, [Date.now()]: { text, work: working } };
+        const newTodos = { ...todos, [Date.now()]: { text, working } };
         setTodos(newTodos);
+        await saveTodos(newTodos);
         setText("");
     };
 
@@ -44,11 +68,13 @@ export default function App() {
                 style={styles.input}
             />
             <ScrollView>
-                {Object.keys(todos).map((key: string) => (
-                    <View style={styles.todo} key={key}>
-                        <Text style={styles.todoText}>{todos[key].text}</Text>
-                    </View>
-                ))}
+                {Object.keys(todos).map((key: string) =>
+                    todos[key].working === working ? (
+                        <View style={styles.todo} key={key}>
+                            <Text style={styles.todoText}>{todos[key].text}</Text>
+                        </View>
+                    ) : null
+                )}
             </ScrollView>
         </View>
     );
